33d036902991d8656c26d6e8e9b824ceb1f90953
Ashutosh Nayak <52618379+Ashutosh1229@users.noreply.github.com>
2025-08-13 09:38:29 +0530

    BS: added lower bound and upper bound

---
+++ BinarySearch/UpperBound.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class UpperBound {
+    public static void main(String[] args) {
+        int[] input = {1,1,2,4,4,6,6,6,8,8,8,11,14,14,19};
+        int item = 8;
+        System.out.println(findLB(input, item));
+    }
+
+    public static int findLB(int[] input, int item){
+        int low = 0;
+        int high = input.length -1;
+        int res = -1;
+        while (low<=high){
+            int mid = low + (high-low)/2;
+            if(input[mid] > item){
+                res = mid;
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        return res-1;
+    }
+}
