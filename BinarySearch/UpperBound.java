6a37a871da7439f08d7ecf217a2f9bc74170ba99
Ashutosh Nayak <52618379+Ashutosh1229@users.noreply.github.com>
2025-08-13 09:40:16 +0530

    BS: fixed upper bound

--- BinarySearch/UpperBound.java
+++ BinarySearch/UpperBound.java
@@ -4,10 +4,10 @@ public class UpperBound {
     public static void main(String[] args) {
         int[] input = {1,1,2,4,4,6,6,6,8,8,8,11,14,14,19};
         int item = 8;
-        System.out.println(findLB(input, item));
+        System.out.println(findUB(input, item));
     }
 
-    public static int findLB(int[] input, int item){
+    public static int findUB(int[] input, int item){
         int low = 0;
         int high = input.length -1;
         int res = -1;
@@ -20,6 +20,6 @@ public class UpperBound {
                 low = mid + 1;
             }
         }
-        return res-1;
+        return res;
     }
 }
