f4bada94576d383dd6e940d1f7c751f748f94ed7
Ashutosh Nayak <52618379+Ashutosh1229@users.noreply.github.com>
2025-08-23 09:33:14 +0530

    SC: added code for time complexity and space complexity.

--- Basics/TimeComplexity.java
+++ Basics/TimeComplexity.java
@@ -6,10 +6,13 @@ import java.util.Date;
 public class TimeComplexity {
 
     public static void main(String[] args) {
-        int inp = 2;  // enter the value to visualize time complexity
+        int inp = 20;  // enter the value to visualize time complexity
         System.out.println(O1(inp));
         System.out.println(ON(inp));
         System.out.println(ON2(inp));
+        System.out.println(OLogN(inp));
+        System.out.println(OAPlusB(inp));
+        System.out.println(OAMultiplyB(inp));
     }
 
     public static String O1(int inp){         // method for 0 1
@@ -36,9 +39,37 @@ public class TimeComplexity {
     }
 
     public static String OLogN(int n){       // method for O log N
-
-
+        int res = 0;
+        for(int i=1; i<n;i=i*2){
+            res = res*i;
+        }
         return "OLogN : done";
+    }
+
+    public static String OAPlusB(int n){       // method for O (a+b)
+        int res = 0;
+        int a = n+1;
+        int b = n*2;
+        for(int i=0; i<a;i++){
+            res = res*i;
+        }
+        for(int i=0; i<b ; i++){
+            res = res*i;
+        }
+        return "O(a+b) : done";
+    }
+
+    public static String OAMultiplyB(int n){       // method for O (a*b)
+        int res = 0;
+        int a = n+1;
+        int b = n*2;
+        for(int i=0; i<a;i++){
+            for(int j=0; j<b ; j++){
+                res = res+j+i;
+            }
+        }
+
+        return "O(a*b) : done";
     }
 
 
